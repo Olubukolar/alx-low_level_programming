bdbec379ea1f496a73fa81f88d99b743414b5a97
olubukolar <ojothomasbukola@gmail.com>
2023-03-16 23:47:22 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#iclude < stdio.h >
+#include <stdio.h>
 /**
  * main - print a line of code using puts
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\n) :
-	return (0) :
+	puts("Programming is like building a multilingual puzzle"\n"):
+	return (0):
 }
