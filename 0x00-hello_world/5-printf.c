4085761cfc52418968bb54d36297a360f334cb88
olubukolar <ojothomasbukola@gmail.com>
2023-03-17 00:21:17 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - print a line of code using printf and not puts
  *
  * Return: 0
-  */
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")'\n';
-	return '(0)' :
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
