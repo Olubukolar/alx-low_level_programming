b6796f3c9f7241e2a8eda3595c5e4ce94820e85f
olubukolar <ojothomasbukola@gmail.com>
2023-03-20 01:26:27 -0700

    Syntax

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error
  * Return: Always 1
  */
 int main(void)
 {
-	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
