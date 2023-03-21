32e83d62e9f2c8d7f817f7191ddef2cd26e27118
olubukolar <ojothomasbukola@gmail.com>
2023-03-21 12:05:00 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - print putchar followed by a new line
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i;
+
+	{
+		_putchar('_');
+		_putchar(p);
+		_putchar(u);
+		_putchar(t);
+		_putchar(c);
+		_putchar(h);
+		_putchar(a);
+		_putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
