b31b234400992d59fcfa87985998ea06362f1858
olubukolar <ojothomasbukola@gmail.com>
2023-03-22 00:30:34 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description: print alphabet in lower case
+ *
+ * Return: Always (0)
+ */
+void print_alphabet(void);
+{
+	int i;
+
+	for int (i = 61; i <= 122; i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+
