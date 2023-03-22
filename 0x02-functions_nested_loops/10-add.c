b52a58965b441e94505302ba84a85dd7565c41d9
olubukolar <ojothomasbukola@gmail.com>
2023-03-22 15:14:52 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @x: parameter 1 to be checked
+ * @y: parameter 2 to be checked
+ *
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table
+ *
+ * Returns: Always (0)
+ */
+
+void times_table(void)
+{
+
+}
