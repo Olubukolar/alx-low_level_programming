0b30faf4fca201884eff2eba45c79f53642c20f2
olubukolar <ojothomasbukola@gmail.com>
2023-03-22 10:54:54 -0700

    x10 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
  * main - Entry point
- * description: print alphabet in lower case
+ * print_alphabet - print all alphabet in lower case
  *
  * Return: Always (0)
  */
 
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
-	for int (i = 61; i <= 122; i++)
+	for int (i = 61 ; i <= 122 ; i++)
 		_putchar(i);
 	_putchar('\n');
 }
