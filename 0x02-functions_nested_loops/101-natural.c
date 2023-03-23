c19e8d8f2483580f8f63592b46b14d26ed7e90aa
olubukolar <ojothomasbukola@gmail.com>
2023-03-22 17:07:08 -0700

    Sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,9 +15,9 @@ int main(void)
 
 	for (i = 0; i < 1024 ; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
+		((i % 3 == 0) || (i % 5 == 0))
 		{
-			 sum = sum + i;
+		 sum = sum + i;
 		}
 	}
 	printf("%d\n", sum);
