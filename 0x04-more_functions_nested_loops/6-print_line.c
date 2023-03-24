c717541b07bd33a1ff6ec8441b1bd40d31b35e8e
olubukolar <ojothomasbukola@gmail.com>
2023-03-24 15:02:35 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: parameter to be checked
+ *
+ * Return: Always (0)
+ */
+void print_line(int n)
+{
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+	for (j = 1 ; j <= n ; j++)
+	{
+		_putchar('-');
+	}
+	       _putchar('\n');
+	}
+}
