68acdef5195e849c72d52d1df00036e6233a8bf0
olubukolar <ojothomasbukola@gmail.com>
2023-03-25 01:40:35 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal shape
+ *
+ * @n: parameter to be checked
+ * Return: Always (0)
+ *
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+
+	else
+	{
+		for (x = 1 ; x < n; x++)
+	{
+		for (y = 1 ; y <= n ; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	}
+}
