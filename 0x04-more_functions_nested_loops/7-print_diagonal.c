3abb7333747ff225a3175b8ae485256650f9106a
olubukolar <ojothomasbukola@gmail.com>
2023-03-25 01:44:11 -0700

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,9 @@ void print_diagonal(int n)
 
 	else
 	{
-		for (x = 1 ; x < n; x++)
+		for (x = 1 ; x <= n; x++)
 	{
-		for (y = 1 ; y <= n ; y++)
+		for (y = 1 ; y < x ; y++)
 		{
 			_putchar(' ');
 		}
