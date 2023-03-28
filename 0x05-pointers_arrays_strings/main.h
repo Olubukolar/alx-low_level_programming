505df010ef75991a88a5da7e1de7a46919fd95a9
olubukolar <ojothomasbukola@gmail.com>
2023-03-28 08:12:41 -0700

    Prints half

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 void reset_to_98(int *n);
 int _putchar(char c);
@@ -9,5 +10,6 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
