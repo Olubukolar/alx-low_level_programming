81ea0e6417fc5a721da767ad52a3db7b47ef2bea
olubukolar <ojothomasbukola@gmail.com>
2023-03-30 09:53:58 -0700

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: Always (0)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while ((s1[i] && s2[2]) != '\0')
+	{
+		if (s1[i] != s2[2])
+		{
+			return s2[i] - s1[i];
+		}
+		i++;
+	}
+	return (0);
+}
