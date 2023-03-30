277d64ba855e41254d41fd2b018081052a18eefa
olubukolar <ojothomasbukola@gmail.com>
2023-03-30 14:48:57 -0700

    Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *string_toupper - function that changes lower case in a string to upper case
+ * @n: parameter
+ * Return: Always (0)
+ */
+char *string_toupper(char *n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	j = 0;
+	while (n[j] != '\0')
+	{
+		if (n[j] >= 'a' && n[j] <= 'z')
+			n[j] = n[j] -32;
+		j++;
+	}
+	return (n);
+}
