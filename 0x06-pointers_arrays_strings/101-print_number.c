c3f062425ff08d4f3421bc4f3b6252cba0df1031
olubukolar <ojothomasbukola@gmail.com>
2023-03-31 14:50:01 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: parameter to be checked
+ * Return: Always (0)
+ */
+void print_number(int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		_putchar ('n');
+		i++;
+	}
+}
