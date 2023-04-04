4149083d6f8cd78dd57e3f30291fffec3eb52b7e
olubukolar <ojothomasbukola@gmail.com>
2023-04-04 12:53:48 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function that sets a pointer to a character
+ * @s: parameter 1
+ * @to: parameter 2
+ * Return: Always (0)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
