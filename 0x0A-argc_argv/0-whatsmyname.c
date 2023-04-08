046cbd88a7acbcf040cc295a0eb13829f3f6ba09
olubukolar <ojothomasbukola@gmail.com>
2023-04-08 15:22:24 -0700

    My new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the function of the prototype
+ * :argc: argument counts
+ * @argv: list the content of command line in argument
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char *argv[]);
+#include <stdio.h>
+int _putchar(char c);
+
+
+
+#endif
