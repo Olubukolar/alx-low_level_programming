529778b117e77fdc4c568f9757d59b2d89aea623
olubukolar <ojothomasbukola@gmail.com>
2023-04-08 15:36:41 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main: function prototype
+ * @argc: argument count
+ * @argv: argument
+ * Return: Always (0)
+ */
+int main(int argc, char* argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
