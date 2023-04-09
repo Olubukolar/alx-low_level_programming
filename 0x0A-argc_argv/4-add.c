e7387e5b353edcaba6685710f08c32bc26819269
olubukolar <ojothomasbukola@gmail.com>
2023-04-09 05:52:31 -0700

    addition of two positive integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,18 +10,20 @@
  */
 int main(int argc, char* argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc >= 1)
+	for (i = 1 ; i < argc ; i++)
 	{
-		for (i = 1 ; i < argc ; i++)
-			sum += atoi(argv[i]);
-		printf("%d\n", sum);
-		return (0);
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
 	}
-	else if ((i != 0) || (i < 0))
-	{
-		printf("Error\n");
-		return (1);
-	}
+	printf("%d\n", sum);
+	return (0);
 }
