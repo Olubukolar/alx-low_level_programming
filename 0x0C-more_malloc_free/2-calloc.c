972100248a205c3784e278b712acb738e586be22
olubukolar <ojothomasbukola@gmail.com>
2023-04-14 10:42:15 -0700

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 
 /**
+ * _memset - function that allocates value
+ * @s: parameter 1
+ * @a: parameter 2
+ * @n: parameter 3
+ * Return: Always (0)
+ */
+char *_memset(char *s, char a, unsigned int n)
+{
+	 char *ptr = s;
+
+	while (n--)
+		*s++ = a;
+	return (ptr);
+}
+
+/**
  * *_calloc - function that allocates memory to an array
  * @nmemb: parameter 1
  * @size: parameter 2
@@ -17,6 +33,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	n = malloc(sizeof(int) * nmemb);
 	if (n == NULL)
 		return (NULL);
+	_memset(n, 0, sizeof(int) * nmemb);
 
 	return (n);
 }
